cd0e22f4080f97383bfe554d545326277bcc1926
Bruno Bento <bruno.bento@delfosim.com>
2024-01-29 19:15:30 -0300

    linter

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,19 @@
 from time import sleep
-from dagster import Definitions, op, job, in_process_executor
+
+from dagster import Definitions, in_process_executor, job, op
 
 sleep(10)
+
 
 @op
 def not_much():
     return
+
 
 @job
 def parallel_job():
     for i in range(10):
         not_much.alias("not_much_" + str(i))()
 
-definitions = Definitions(
-    executor=in_process_executor,
-    jobs=[parallel_job]
-)
+
+definitions = Definitions(executor=in_process_executor, jobs=[parallel_job])
